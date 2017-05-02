bf6e859c67fc12f646b5b1f0725183beebeed4ac
Luke Brady <lbrad23105@gmail.com>
2017-05-02 15:15:40 -0400

    First commit to GitHub.

---
+++ Configure-System.ps1
@@ -0,0 +1,80 @@
+﻿$user = "someuser"
+$pass = ConvertTo-SecureString "password" -AsPlainText -Force
+$cred = New-Object -TypeName System.Management.Automation.PSCredential($user,$pass)
+
+function Get-VMMetaData
+{
+    [CmdletBinding()]
+    Param 
+    (
+        # Elasticsearch URI.
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=1)]
+        $URI
+    )
+    
+    $vmMetaDataObj = $(Invoke-RestMethod -Method Get -Uri $URI -ErrorAction Stop)._source
+    $vmMetaDataJSON = $vmMetaDataObj | ConvertTo-Json
+    
+    $vmMetaData = @{
+        Object = $vmMetaDataObj;
+        JSON = $vmMetaDataJSON
+    }
+    Invoke-RestMethod -Method Delete -Uri $URI -ErrorAction Stop
+    return $vmMetaData       
+}
+
+function Change-NetworkSettings 
+{
+    [CmdletBinding()]
+    Param
+    (
+        # Object that will used to configure network.
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $VMMetaData
+    )
+    
+    $ifaceIndex = $(Get-NetAdapter -Name "Ethernet").ifIndex
+    
+    try 
+    {
+        New-NetIPAddress -IPAddress $VMMetaData.Object.IP_Addr `
+                         -DefaultGateway $VMMetaData.Object.Gateway `
+                         -InterfaceIndex $ifaceIndex 
+        Set-DnsClientServerAddress -InterfaceIndex $ifaceIndex `
+                                   -ServerAddresses $VMMetaData.Object.DNS_Servers
+    }
+    catch { $Error }   
+}
+
+function Join-Domain
+{
+    [CmdletBinding()]
+    Param
+    (
+        # Domain that will be joined.
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        [String]
+        $Domain,
+        # Name that instance will be changed to.
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=1)]
+        [String]
+        $ComputerName
+    )
+    
+    try 
+    {
+        Rename-Computer -NewName $ComputerName -DomainCredential $cred -ErrorAction Stop
+        Add-Computer -DomainName $Domain -Credential $cred -ErrorAction Stop -Restart
+    } 
+    catch { $Error }
+}
+
+$data = Get-VMMetaData -URI "http://168.30.194.202:9200/vcloud/hosts/latest"
---
+++ VCloud.ps1
@@ -0,0 +1,176 @@
+﻿if ( !(Get-Module -Name VMware.VimAutomation.Core -ErrorAction SilentlyContinue) ) {
+. “C:\Program Files (x86)\VMware\Infrastructure\vSphere PowerCLI\Scripts\Initialize-PowerCLIEnvironment.ps1”
+}
+
+$user = "someuser"
+$pass = ConvertTo-SecureString "password" -AsPlainText -Force
+$cred = New-Object -TypeName System.Management.Automation.PSCredential($user,$pass)
+Connect-VIServer -Server $server -Credential $cred
+
+function Get-LeastLoadedDataStore 
+{
+    $datastores = Get-Datastore
+    
+    $spaceObject = $datastores.FreeSpaceGB | Sort-Object
+    $leastFreeSpace = $spaceObject[$spaceObject.Length - 1]
+    $leastLoaded = @{}
+
+    foreach($datastore in $datastores) 
+    {
+        try
+        {
+            if($datastore.FreeSpaceGB -eq $leastFreeSpace)
+            {
+                $leastLoaded.Add($datastore.Name,$leastFreeSpace)
+            } 
+         } 
+         catch { $Error }   
+    }
+
+    return $leastLoaded
+}
+
+function Get-DCMetaData 
+{
+    $datacenter = Get-Datacenter
+    $hosts = $(Get-VMHost).Name
+    $templates = Get-Template
+    $memory = $(Get-ResourcePool).MemReservationGB
+
+    $dcMetaData = @{
+        "Datacenter" = $datacenter;
+        "Hosts" = $hosts;
+        "Templates" = $templates;
+        "Memory" = $memory;
+    }
+
+    return $dcMetaData
+}
+
+function Document-Deployment
+{
+    [CmdletBinding()]
+    Param
+    (
+        # Object of MetaData collected by Get-DCMetaData
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $MetaDataObject,
+        [String]
+        $OutputPath,
+        # ElasticSearch instance to send documentation.
+        [String]
+        $ElasticSearchURI
+    )
+    
+
+}
+
+function New-VMFromTemplate
+{
+    [CmdletBinding()]
+    Param
+    (
+        # JSON template path.
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $Template
+    )
+
+    $templateObject = Get-Content -Path $Template | ConvertFrom-Json -Verbose
+    $name = $($templateObject.Name).toUpper()
+    $datastore = Get-LeastLoadedDataStore
+    
+    if($(Get-VM $name) -eq $null) 
+    {
+        New-VM -Name $name `
+               -Template $templateObject.Template `
+               -ResourcePool $templateObject.Resource_Pool `
+               -Datastore $templateObject.Datastore
+        if($(Get-VM $name).PowerState -eq "PoweredOn") 
+        {
+            Write-Host "The VM, $name is already powered on."
+        }
+        else 
+        {
+            Write-Host "Powering on $name."
+            Start-VM -VM $name
+        }
+    }
+    else 
+    {
+        Write-Output "This VM has already been provisioned."
+        if($(Get-VM $name).PowerState -eq "PoweredOn") 
+        {
+            Write-Host "The VM, $name is already powered on."
+        }
+        else 
+        {
+            Write-Host "Powering on $name."
+            Start-VM -VM $name
+        }
+    }
+
+    return $templateObject
+}
+
+function Set-VMMetaData 
+{
+    [CmdletBinding()]
+    Param 
+    (
+        # JSON template path.
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $VMTemplateObject,
+        # Elasticsearch URI.
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=1)]
+        $URI
+     )
+     $uploadTime = Get-Date -UFormat %H:%M:%S
+     $vmMetaDataObj = @{
+        Name = $VMTemplateObject.name.toUpper();
+        IP_Addr = $VMTemplateObject.args.ip_addr;
+        Netmask = $VMTemplateObject.args.netmask;
+        Gateway = $VMTemplateObject.args.gateway;
+        DNS_Servers = $VMTemplateObject.args.dns_servers;
+        Domain = $VMTemplateObject.args.domain;
+        Upload_Time = $uploadTime;   
+     }
+
+     $vmMetaDataJSON = $vmMetaDataObj | ConvertTo-Json -ErrorAction Stop
+     $vmMetaDataJSON
+     Invoke-RestMethod -Method Post -Uri $URI -Body $vmMetaDataJSON -ErrorAction Stop
+}
+
+function Get-VMMetaData
+{
+    [CmdletBinding()]
+    Param 
+    (
+        # Elasticsearch URI.
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=1)]
+        $URI
+    )
+    
+    $vmMetaDataObj = $(Invoke-RestMethod -Method Get -Uri $URI -ErrorAction Stop)._source
+    $vmMetaDataJSON = $vmMetaDataObj | ConvertTo-Json
+    
+    $vmMetaData = @{
+        Object = $vmMetaDataObj;
+        JSON = $vmMetaDataJSON
+    }
+    
+    return $vmMetaData       
+}
+
+New-VMFromTemplate -Template C:\Users\ltbrady\Dropbox\Software\PowerShell\vCloud\testtemp.json -OutVariable temp
+Set-VMMetaData -VMTemplateObject $temp -URI "http://dev-elkstack.ung.co:9200/vcloud/hosts/latest"
+# $data = Get-VMMetaData -URI "http://dev-elkstack.ung.co:9200/vcloud/hosts/latest"
